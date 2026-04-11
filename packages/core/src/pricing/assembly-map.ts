05da8b092abae5b1c415283e373aa3599d94137d
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-04-11 13:33:36 -0700

    feat(core): add part sub-assembly category map

---
+++ packages/core/src/pricing/assembly-map.ts
@@ -0,0 +1,49 @@
+export const ASSEMBLY_MAP: Record<string, string[]> = {
+  front_bumper: [
+    "bumper_cover",
+    "bumper_reinforcement",
+    "energy_absorber",
+    "grille",
+    "grille_molding",
+  ],
+  rear_bumper: [
+    "bumper_cover_rear",
+    "bumper_reinforcement_rear",
+    "energy_absorber_rear",
+    "bumper_step_pad",
+  ],
+  hood: [
+    "hood_panel",
+    "hood_latch",
+    "hood_hinge_left",
+    "hood_hinge_right",
+    "hood_insulator",
+  ],
+  front_door_left: [
+    "door_shell_fl",
+    "door_glass_fl",
+    "door_regulator_fl",
+    "door_handle_outer_fl",
+    "door_mirror_fl",
+  ],
+  front_door_right: [
+    "door_shell_fr",
+    "door_glass_fr",
+    "door_regulator_fr",
+    "door_handle_outer_fr",
+    "door_mirror_fr",
+  ],
+  quarter_panel_left: [
+    "quarter_panel_outer_l",
+    "wheel_arch_molding_rl",
+    "rocker_panel_l",
+  ],
+  quarter_panel_right: [
+    "quarter_panel_outer_r",
+    "wheel_arch_molding_rr",
+    "rocker_panel_r",
+  ],
+  windshield: ["windshield_glass", "windshield_molding", "rain_sensor"],
+  fender_left: ["fender_panel_l", "fender_liner_l", "fender_molding_l"],
+  fender_right: ["fender_panel_r", "fender_liner_r", "fender_molding_r"],
+};
